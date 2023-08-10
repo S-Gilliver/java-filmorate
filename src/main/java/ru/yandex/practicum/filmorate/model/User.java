07d565a484648ba0d4565234b0d598a72665c2c3
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-08-11 02:20:10 +0300

    sprint 9

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
-@Builder
 public class User {
 
     private Integer id;
