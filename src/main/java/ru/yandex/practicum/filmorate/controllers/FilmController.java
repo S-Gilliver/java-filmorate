c79b9a5b63d71079746adfaf28d808f2f37f153f
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-08-11 22:51:18 +0300

    sprint 9

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,51 +1,58 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    private final Map<Integer, Film> filmsMap = new HashMap<>();
+    private final Map<Integer, Film> films = new HashMap<>();
     private int id = 1;
 
-    @GetMapping()
+    @GetMapping
     public Collection<Film> getFilms() {
-        return filmsMap.values();
+        return films.values();
     }
 
-    @PostMapping()
+    @PostMapping
     public Film addFilm(@RequestBody @Valid Film film) {
-        filmValidator(film);
+        validateFilm(film);
         film.setId(generateId());
-        filmsMap.put(film.getId(), film);
-        log.info("Фильм успешно добавлен!");
+        films.put(film.getId(), film);
+        log.info("The movie has been added successfully!");
         return film;
     }
 
-    @PutMapping()
-    public Film updateFilm(@RequestBody @Valid Film film) throws NotFoundException {
-        if (!filmsMap.containsKey(film.getId())) {
-            throw new NotFoundException("Фильм с id " + film.getId() + " не существует!");
+    @PutMapping
+    public Film updateFilm(@RequestBody @Valid Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new NotFoundException("The movie with id " + film.getId() + " does not exist!");
         }
-        filmValidator(film);
-        filmsMap.put(film.getId(), film);
-        log.info("Фильм успешно обновлен!");
+        validateFilm(film);
+        films.put(film.getId(), film);
+        log.info("The movie has been successfully updated!");
         return film;
     }
 
-    private void filmValidator(Film film) throws ValidationException {
+    private void validateFilm(Film film) throws ValidationException {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Кино еще не изобрели!");
+            throw new ValidationException("Cinema hasn't been invented yet!");
         }
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,51 +1,58 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final Map<Integer, User> usersMap = new HashMap<>();
+    private final Map<Integer, User> users = new HashMap<>();
     private int id = 1;
 
     @GetMapping
     public Collection<User> getUsers() {
-        return usersMap.values();
+        return users.values();
     }
 
     @PostMapping
     public User addUser(@RequestBody @Valid User user) {
         userValidator(user);
         user.setId(generateId());
-        usersMap.put(user.getId(), user);
-        log.info("Пользователь успешно добавлен!");
+        users.put(user.getId(), user);
+        log.info("The user has been successfully added!");
         return user;
     }
 
     @PutMapping
     public User updateUser(@RequestBody @Valid User user) {
-        if (!usersMap.containsKey(user.getId())) {
-            throw new NotFoundException("Пользователь с id " + user.getId() + " не существует!");
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("The user with id " + user.getId() + " does not exist!");
         }
         userValidator(user);
-        usersMap.put(user.getId(), user);
-        log.info("Данные о пользователе успешно обновлены!");
+        users.put(user.getId(), user);
+        log.info("User data has been successfully updated!");
         return user;
     }
 
     private void userValidator(User user) throws ValidationException {
         if (user.getLogin().contains(" ")) {
-            throw new BadRequestException("Логин не может содержать пробелы!");
+            throw new BadRequestException("The login cannot contain spaces!");
         }
         if ((user.getName() == null) || user.getName().isEmpty()) {
             user.setName(user.getLogin());
